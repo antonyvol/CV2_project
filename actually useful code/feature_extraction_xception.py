7622f0fa851eaff50479e66cfdc9157e5d3e0e11
Anton Volkov <antonyvol1@gmail.com>
2019-04-30 17:30:31 +0200

    commit all useful and useless code

--- actually useful code/feature_extraction_xception.py
+++ actually useful code/feature_extraction_xception.py
@@ -3,6 +3,7 @@ slim = tf.contrib.slim
 
 import numpy as np
 from skimage import transform, io
+import h5py
 
 # ------------ READ AND PREPROCESS (FOR XCEPTION) IMAGE BY IMAGE --------------
 # ..\\cv2_data\\train\\images\\0001.jpg
@@ -21,18 +22,30 @@ def read_single_image(path):
 
 # ------------------ ACTUALLY XCEPTION ----------------------
 
+weights = h5py.File('xception_weights_tf_dim_ordering_tf_kernels.h5', 'r')
+
+# Get the data
+# data = weights['convolution2d_1'].values()
+# print(data)
+
+
 x_in = tf.placeholder(tf.float32, shape=(1, 299, 299, 3))
 
-block1_conv1 = tf.nn.conv2d(input=x_in, filter=(3, 3, 3, 32), strides=(1, 2, 2, 1), padding='VALID')
+cur_weights = []
+weights['convolution2d_1'].items()
+print(cur_weights)
+# block1_conv1_filters = tf.Variable(, shape=)
+# block1_conv1 = tf.nn.conv2d(input=x_in, filter=block1_conv1_filters, strides=(1, 2, 2, 1), padding='VALID')
 
 
 
 
 # -------------------------- code to test stuff in progress ----------------
+# init_op = tf.global_variables_initializer()
 
-with tf.Session() as sess:
-	data = read_single_image('..\\cv2_data\\train\\images\\0001.jpg')
-	x = sess.run(block1_conv1, feed_dict={x_in: data})
-	print(x)
+# with tf.Session() as sess:
+# 	data = read_single_image('..\\cv2_data\\train\\images\\0001.jpg')
+# 	x = sess.run(block1_conv1, feed_dict={x_in: data})
+# 	print(x)
 
 
