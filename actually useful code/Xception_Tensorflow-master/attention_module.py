bd31e2de6ddb4ba4f580d731514f267a084430d4
Angelie Kraft <ankraft@uni-bremen.de>
2019-05-19 21:08:35 +0200

    Continued with reading all data and tried to implement the learning -> not really working yet

--- actually useful code/Xception_Tensorflow-master/attention_module.py
+++ actually useful code/Xception_Tensorflow-master/attention_module.py
@@ -7,6 +7,7 @@ import scipy
 from feature_extractor import extract_features
 import tensorflow as tf
 import os
+import matplotlib.pyplot as plt
 
 from keras.layers import Dense, Conv2D
 from keras.layers.core import Activation
@@ -20,7 +21,7 @@ batch_size = 128
 epochs = 10
 
 # Load feature maps
-#train_features_np = np.load("/Users/Angelie/Google Drive/train_data_features.npy")
+train_features_np = np.load("/Users/Angelie/Google Drive/train_data_features.npy")
 test_features_np = np.load("/Users/Angelie/Google Drive/test_data_features.npy")
 val_features_np = np.load("/Users/Angelie/Google Drive/validation_data_features.npy")
 
@@ -37,14 +38,15 @@ def load_labels(directory):
         np_image /= 127.5
         np_image -= 1.
         return np_image
-    #labels = np.array([load_label(img) for img in directory])
-    print(load_label(directory))
+    listing = os.listdir(directory)
+    labels = np.array([load_label(directory + img) for img in listing])
+    #print(load_label(directory))
     #out_arr = np.concatenate(np.squeeze(np.asarray(labels))[4:], axis=2)
-    #return tf.convert_to_tensor(out_arr)
+    return tf.convert_to_tensor(labels)
 
-train_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/train/fixations/0001.jpg')
+train_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/train/fixations/')
 
-#val_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/val/fixations')
+val_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/val/fixations/')
 
 # Create 2D normal continuous distribution
 def gaussian2d(dim, mean=0, std=1):
@@ -66,19 +68,18 @@ model.add(weighted)
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
-'''
-model.fit(x_train, y_train,
+
+model.fit(train_features_np, train_labels,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test, y_test),
-          callbacks=[history])
-score = model.evaluate(x_test, y_test, verbose=0)
+          validation_data=(val_features_np, val_labels))
+score = model.evaluate(val_features_np, val_labels, verbose=0)
 
 
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-plt.plot(range(1, 11), history.acc)
+#plt.plot(range(1, 11), history.acc)
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
-plt.show()'''
+plt.show()
