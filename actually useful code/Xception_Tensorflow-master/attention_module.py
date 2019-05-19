05b686d0b7c41abfcd4be3b66f1240b91513d9bb
Angelie Kraft <ankraft@uni-bremen.de>
2019-05-19 19:07:38 +0200

    - now loads fixation images correctly
    - problem: numpy file with training features too large! (max. 2GB supported by convert_to_tensor function)

--- actually useful code/Xception_Tensorflow-master/attention_module.py
+++ actually useful code/Xception_Tensorflow-master/attention_module.py
@@ -6,6 +6,7 @@ import scipy.stats as st
 import scipy
 from feature_extractor import extract_features
 import tensorflow as tf
+import os
 
 from keras.layers import Dense, Conv2D
 from keras.layers.core import Activation
@@ -15,7 +16,35 @@ from EltWiseProd import EltWiseProd
 
 # Some constants
 no_of_filters = 728 * 2
+batch_size = 128
+epochs = 10
 
+# Load feature maps
+#train_features_np = np.load("/Users/Angelie/Google Drive/train_data_features.npy")
+test_features_np = np.load("/Users/Angelie/Google Drive/test_data_features.npy")
+val_features_np = np.load("/Users/Angelie/Google Drive/validation_data_features.npy")
+
+#train_features = tf.convert_to_tensor(train_features_np)
+test_features = tf.convert_to_tensor(test_features_np)
+val_features = tf.convert_to_tensor(val_features_np)
+
+# Load fixation maps
+def load_labels(directory):
+    def load_label(img):
+        np_image = scipy.misc.imread(img)
+        np_image = scipy.misc.imresize(np_image, (299, 299))
+        np_image = np.expand_dims(np_image, axis=0).astype(np.float32)
+        np_image /= 127.5
+        np_image -= 1.
+        return np_image
+    #labels = np.array([load_label(img) for img in directory])
+    print(load_label(directory))
+    #out_arr = np.concatenate(np.squeeze(np.asarray(labels))[4:], axis=2)
+    #return tf.convert_to_tensor(out_arr)
+
+train_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/train/fixations/0001.jpg')
+
+#val_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/val/fixations')
 
 # Create 2D normal continuous distribution
 def gaussian2d(dim, mean=0, std=1):
@@ -24,10 +53,7 @@ def gaussian2d(dim, mean=0, std=1):
     gaussian2d = np.outer(gaussian1d, gaussian1d)
     return gaussian2d
 
-feature_map = np.load("/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/validation_data_features.npy")
-
-features = tf.convert_to_tensor(feature_map)
-
+# Network model
 model = Sequential()
 model.add(Conv2D(63, kernel_size=(3,3)))
 model.add(Conv2D(1, kernel_size=(1,1)))
@@ -35,6 +61,7 @@ pierced = Activation('relu')
 prior = gaussian2d(17,std=0.5)
 weighted = EltWiseProd([pierced, prior])
 model.add(weighted)
+
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
