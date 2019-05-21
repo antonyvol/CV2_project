7bd5a7a29492e8ada601a45515cd739c4fc159e3
Angelie Kraft <ankraft@uni-bremen.de>
2019-05-21 15:57:47 +0200

    - Using Multiply() layer instead of custom EltWiseProd now - Some dtype fixes - Temporarily only loading 700 cases of the training data -> TODO: introduce batch selection
    ... fixes over fixes still not all fixed

--- actually useful code/Xception_Tensorflow-master/EltWiseProd.py
+++ actually useful code/Xception_Tensorflow-master/EltWiseProd.py
@@ -1,6 +1,7 @@
 from keras import backend as K
 from keras.layers import Layer
 import numpy as np
+import tensorflow as tf
 
 
 class EltWiseProd(Layer):
@@ -10,14 +11,14 @@ class EltWiseProd(Layer):
         super(EltWiseProd, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        assert isinstance(input_shape, list)
-        if input_shape[-1] is None:
-            raise ValueError('Axis ' +  + ' of '
-                             'input tensor should have a defined dimension '
-                             'but the layer received an input with shape ' +
-                             str(input_shape) + '.')
-        self.input_spec = InputSpec(ndim=len(input_shape),
-                                    axes=dict(list(enumerate(input_shape[1:], start=1))))
+        #assert isinstance(input_shape, list)
+        #if input_shape[-1] is None:
+        #    raise ValueError('Axis ' +  + ' of '
+        #                     'input tensor should have a defined dimension '
+        #                     'but the layer received an input with shape ' +
+        #                     str(input_shape) + '.')
+        #self.input_spec = InputSpec(ndim=len(input_shape),
+        #                            axes=dict(list(enumerate(input_shape[1:], start=1))))
         
         # Create a trainable weight variable for this layer.
         # self.kernel = self.add_weight(name='kernel', shape=(input_shape[1], self.output_dim), initializer='uniform' trainable=True)
@@ -26,12 +27,14 @@ class EltWiseProd(Layer):
     #    self.trainable = False
 
     def call(self, x):
-        assert isinstance(x, list)
-        a, b = x
-        return np.multiply(a, b)
+        #assert isinstance(x, list)
+        ls = np.array([])
+        tf.map_fn(lambda y: np.append(ls, y), x)
+        a, b = ls
+        return SparseTensor(x.indices, np.multiply(a, b))
 
     def compute_output_shape(self, input_shape):
-        assert isinstance(input_shape, list)
+        #assert isinstance(input_shape, list)
         shape_a, shape_b = input_shape
         return [(shape_a[0], self.output_dim), shape_b[:-1]]
 
--- actually useful code/Xception_Tensorflow-master/attention_module.py
+++ actually useful code/Xception_Tensorflow-master/attention_module.py
@@ -9,11 +9,8 @@ import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
 
-from keras.layers import Dense, Conv2D
-from keras.layers.core import Activation
-from keras.models import Sequential
-
-from EltWiseProd import EltWiseProd
+from keras.layers import Conv2D, Input, Multiply
+from keras.models import Model
 
 # Some constants
 no_of_filters = 728 * 2
@@ -25,15 +22,18 @@ train_features_np = np.load("/Users/Angelie/Google Drive/train_data_features.npy
 test_features_np = np.load("/Users/Angelie/Google Drive/test_data_features.npy")
 val_features_np = np.load("/Users/Angelie/Google Drive/validation_data_features.npy")
 
-#train_features = tf.convert_to_tensor(train_features_np)
+
+train_features = tf.convert_to_tensor(train_features_np[:700])
 test_features = tf.convert_to_tensor(test_features_np)
 val_features = tf.convert_to_tensor(val_features_np)
+
+# TODO: batch wise
 
 # Load fixation maps
 def load_labels(directory):
     def load_label(img):
         np_image = scipy.misc.imread(img)
-        np_image = scipy.misc.imresize(np_image, (299, 299))
+        np_image = scipy.misc.imresize(np_image, (19, 19))
         np_image = np.expand_dims(np_image, axis=0).astype(np.float32)
         np_image /= 127.5
         np_image -= 1.
@@ -45,6 +45,7 @@ def load_labels(directory):
     return tf.convert_to_tensor(labels)
 
 train_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/train/fixations/')
+train_labels = train_labels[:700]
 
 val_labels = load_labels('/Users/Angelie/Documents/Universitaet/IAS/SS19/CV2/Eye_fixation_project/cv2_data/val/fixations/')
 
@@ -53,28 +54,29 @@ def gaussian2d(dim, mean=0, std=1):
     x = np.linspace(-1, 1, dim+1)
     gaussian1d = np.diff(st.norm.cdf(x, loc=mean, scale=std))
     gaussian2d = np.outer(gaussian1d, gaussian1d)
-    return gaussian2d
+    return tf.convert_to_tensor(gaussian2d, dtype=tf.float32)
+
 
 # Network model
-model = Sequential()
-model.add(Conv2D(63, kernel_size=(3,3)))
-model.add(Conv2D(1, kernel_size=(1,1)))
-pierced = Activation('relu')
-prior = gaussian2d(17,std=0.5)
-weighted = EltWiseProd([pierced, prior])
-model.add(weighted)
 
+# TODO: flatten?
+inputs = Input(shape=(19,19,1))
+saliency_layer = Conv2D(63, kernel_size=(3,3), padding="same")(inputs)
+piercing_layer = Conv2D(1, kernel_size=(1,1))(saliency_layer)
+outputs = Multiply()([piercing_layer, piercing_layer])
+
+model = Model(inputs=inputs, outputs=outputs)
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
-model.fit(train_features_np, train_labels,
+model.fit(train_features, train_labels,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(val_features_np, val_labels))
-score = model.evaluate(val_features_np, val_labels, verbose=0)
+          validation_data=(val_features, val_labels))
+score = model.evaluate(val_features, val_labels, verbose=0)
 
 
 print('Test loss:', score[0])
